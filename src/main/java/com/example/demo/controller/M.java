093606d430d22d4851efadcabc18c45415de665c
Aleksandr Sologub <aleksandr_sologub@epam.com>
2024-05-16 14:57:24 +0300

    test

---
+++ src/main/java/com/example/demo/controller/M.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.dto.MessageDto;
+import com.example.demo.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/api/message")
+public class M {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping
+    public ResponseEntity<?> saveOrGetMessage(@RequestBody MessageDto message) {
+        if (!message.getMessage().startsWith("history ")) {
+            return ResponseEntity.ok(messageService.saveMessage(message));
+        } else {
+            List<String> messages;
+            try {
+              messages = messageService.getMessages(message);
+            } catch (UsernameNotFoundException e) {
+                throw e;
+            }
+           return ResponseEntity.ok(messages);
+        }
+
+    }
+}
